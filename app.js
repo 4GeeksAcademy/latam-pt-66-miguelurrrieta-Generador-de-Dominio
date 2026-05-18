e4b4e6f49faa488fda8ed1cb8eae3d7ab9da66d5
miguelurrieta166 <miguelurrieta166@gmail.com>
2026-05-18 20:32:30 +0000

    generador listo

---
+++ app.js
@@ -0,0 +1,17 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+
+for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+        for (let n = 0; n < noun.length; n++) {
+           
+            
+            let domainName = pronoun[p] + adj[a] + noun[n] + ".com";
+           
+            
+            console.log(domainName);
+        }
+    }
+}
